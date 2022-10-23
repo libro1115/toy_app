b35891905c99c7f993d4176449876ae9ef5dcdb1
libro1115 <libro.1115@gmail.com>
2022-10-23 18:13:53 +0900

    ログイン・ログアウトのテスト

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,14 +4,35 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
-  test "login with invalid infomation" do
+  test "login with valid email/invalid password" do
     get login_path
-    assert_template 'sessions/new'
-    post login_path, params:{session:{email:"",password:""}}
+    assert_template'sessions/new'
+    post login_path,params:{session:{email:@user.email,password:"invalid"}}
+    assert_not is_logged_in?
+    asseert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with invalid infomation followed by logout" do
+    post login_path, params:{session:{email:@user.email,password:'password'}}
+    assert is_logged_in?
+    assert_redirect_to @user
+    follow_rediirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path,count:0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirect_to root_url
+    follow_redirect! 
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,count:0
+    assert_select "a[href=?]", user_path(@user),count:0
   end
   # test "the truth" do
   #   assert true
