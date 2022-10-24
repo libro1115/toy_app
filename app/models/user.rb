39d8c39ed93569941ac4be07a0fcfe05e8511448
libro1115 <libro.1115@gmail.com>
2022-10-24 10:21:01 +0900

    複数個所からのログアウト時のエラー回避

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,7 @@ class User < ApplicationRecord
   end
   #渡されたトークンが一致したらtrue
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   #ログイン破棄
